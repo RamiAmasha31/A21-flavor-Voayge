916c27a4649a04dcebd401fc226d1c2eb7d9cc2e
RamiAmasha31 <ramiamasha84@gmail.com>
2024-03-22 19:03:38 +0200

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://ramiamasha31.github.io/A21-flavor-Voayge/',
   plugins: [react()],
   build: {
     outDir: resolve(__dirname, 'docs'), // Change the output directory to 'docs'
